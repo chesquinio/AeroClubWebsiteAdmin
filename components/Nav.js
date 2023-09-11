78a5456caf28ccdae7ebe1562817f3c9477d1c5b
francis <willenerfrancis0@gmail.com>
2023-09-11 18:17:57 -0300

    Boton Cerrar Sesion

--- components/Nav.js
+++ components/Nav.js
@@ -2,19 +2,29 @@ import Link from "next/link"
 import {useRouter} from "next/router";
 
 function Nav() {
-  const inactiveLink = 'flex my-2 p-1 px-5'
-  const activeLink = inactiveLink+' bg-primary rounded-md text-white font-medium'
+  const inactiveLink = 'flex my-2 p-1 px-5 hover:bg-primary hover:text-white rounded-md transition-all duration-500'
+  const activeLink = inactiveLink+' bg-primary text-white font-medium'
   const router = useRouter()
   const {pathname} = router
 
+  const logoutSession = () => {
+    localStorage.removeItem("token"); 
+    router.push("/iniciar"); 
+  }
+
   return (
     <aside>
-        <nav className="bg-white flex flex-col mt-5 ml-8 p-5 rounded-lg font-light text-xl">
-            <Link className={pathname === '/' ? activeLink : inactiveLink} href={'/'}>Inicio</Link>
-            <Link className={pathname === '/noticias' || pathname === '/noticias/nueva' || pathname === '/noticias/editar/[id]' ? activeLink : inactiveLink} href={'/noticias'}>Noticias</Link>
-            <Link className={pathname === '/aeronautica' ? activeLink : inactiveLink} href={'/aeronautica'}>Aeronáutica</Link>
-            <Link className={pathname === '/tenis' ? activeLink : inactiveLink} href={'/tenis'}>Tenis</Link>
-            <Link className={pathname === '/parque' || pathname === '/parque/colonia' || pathname === '/parque/colonia/[documento]' ? activeLink : inactiveLink} href={'/parque'}>Parque</Link>
+        <nav className="flex flex-col mt-5 ml-8 font-light text-xl">
+            <div className="bg-white rounded-lg p-5">
+              <Link className={pathname === '/' ? activeLink : inactiveLink} href={'/'}>Inicio</Link>
+              <Link className={pathname === '/noticias' || pathname === '/noticias/nueva' || pathname === '/noticias/editar/[id]' ? activeLink : inactiveLink} href={'/noticias'}>Noticias</Link>
+              <Link className={pathname === '/aeronautica' ? activeLink : inactiveLink} href={'/aeronautica'}>Aeronáutica</Link>
+              <Link className={pathname === '/tenis' ? activeLink : inactiveLink} href={'/tenis'}>Tenis</Link>
+              <Link className={pathname === '/parque' || pathname === '/parque/colonia' || pathname === '/parque/colonia/[documento]' ? activeLink : inactiveLink} href={'/parque'}>Parque</Link>
+            </div>
+            <div className="bg-white rounded-lg py-2 px-5 mt-5">
+              <button onClick={logoutSession} className={inactiveLink}>Cerrar Sesión</button>
+            </div>
         </nav>
     </aside>
   )
--- components/ResponsiveNav.js
+++ components/ResponsiveNav.js
@@ -1,7 +1,14 @@
 import Link from 'next/link'
-import React from 'react'
+import {useRouter} from "next/router";
 
 function ResponsiveNav() {
+  const router = useRouter()
+
+  const logoutSession = () => {
+    localStorage.removeItem("token"); 
+    router.push("/iniciar"); 
+  }
+
   return (
     <div>
         <nav className='flex flex-col items-center bg-primary w-screen'>
@@ -10,6 +17,7 @@ function ResponsiveNav() {
             <Link className='text-xl font-medium py-5 text-white hover:text-gray-200 transition' href={'/aeronautica'}>Aeronáutica</Link>
             <Link className='text-xl font-medium py-5 text-white hover:text-gray-200 transition' href={'/tenis'}>Tenis</Link>
             <Link className='text-xl font-medium py-5 text-white hover:text-gray-200 transition' href={'/parque'}>Parque</Link>
+            <button onClick={logoutSession} className='text-xl font-medium py-5 text-white hover:text-gray-200 transition'>Cerrar Sesión</button>
         </nav>
     </div>
   )
